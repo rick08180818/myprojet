12affe060f455914819f9ed909f9662dc99fc362
rick08180818 <rick08180818@gmail.com>
2023-02-20 13:50:50 +0800

    Initial commit

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,15 @@
+package com.tom
+
+fun main(array: Array<String>){
+//    println("Hello kotlin")
+//    Person().hello()
+    val h = Person()
+    h.hello()
+}
+
+
+class Person{
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
